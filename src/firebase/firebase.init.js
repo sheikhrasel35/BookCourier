a192401a7a61469f54f06849a026621c02546268
Sheikh Rasel <sheikhrasel35@gmail.com>
2026-04-18 23:02:59 +0600

    add the firebase.init.js file

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyDvRRIJxKxSOFDxtvJXCyyOC_kPwOiCQ5U",
+  authDomain: "bookcourier-b525d.firebaseapp.com",
+  projectId: "bookcourier-b525d",
+  storageBucket: "bookcourier-b525d.firebasestorage.app",
+  messagingSenderId: "339183540842",
+  appId: "1:339183540842:web:9f55677334a08620274813",
+  measurementId: "G-XGHGG6M7C3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
